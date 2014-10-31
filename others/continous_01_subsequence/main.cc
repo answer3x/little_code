c78c0f3be43e4bb0db81b8ffc288bdbbebf6806c
姚奕彦 <yaoyiyan@ganji.com>
2014-10-31 13:37:16 +0800

    =use list, the code is more beautiful

--- others/continous_01_subsequence/main.cc
+++ others/continous_01_subsequence/main.cc
@@ -1,73 +1,52 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include <cassert>
 using namespace std;
 
 
 class Solution {
-  private:
-    class Pos {
-      public:
-      Pos(int l, int r) : 
-          left(l), right(r) {}
-      Pos(){}
-      int left;
-      int right;
-    };
-
-    void deal(int sum, int i, 
-              vector<Pos> &zero,
-              vector<Pos> &positive, 
-              vector<Pos> &negative) {
-      if (sum == 0) {
-        if (zero.empty()) {
-          zero.push_back(Pos(-1, i));
-        } else {
-          zero[0].right = i;
-        }
-      } else if (sum > 0) {
-        if (sum >= positive.size()) {
-          positive.push_back(Pos(i, i));
-        } else {
-          positive[sum].right = i;
-        }
-      } else {
-        if (-sum >= negative.size()) {
-          negative.push_back(Pos(i, i));
-        } else {
-          negative[-sum].right = i;
-        }
-      }
-    }
-    
   public:
     int cal(const vector<int> &vec) {
       if (vec.empty()) return 0;
 
-      vector<Pos> zero;
-      vector<Pos> positive(1, Pos()); 
-      vector<Pos> negative(1, Pos());
+      list<pair<int, int> > pos(1, make_pair(-1, -1));
+      list<pair<int, int> >::iterator iter = pos.begin();
 
       int sum = 0;
       for (size_t i = 0; i < vec.size(); ++i) {
-        sum += (vec[i] == 1 ? 1 : -1);
-        deal(sum, i, zero, positive, negative);
+
+        if (vec[i] == 1) {
+          sum += 1;
+          list<pair<int, int> >::iterator tmp = iter;
+          if (++tmp == pos.end()) {
+            pos.push_back(make_pair(i, i));
+            ++iter;
+          } else {
+            ++iter;
+            iter->second = i;
+          }
+
+        } else { // vec[i] == -1
+          sum -= 1;
+          if (iter == pos.begin()) {
+            pos.push_front(make_pair(i, i));
+            --iter;
+          } else {
+            --iter;
+            iter->second = i;
+          }
+        }
+
       }
 
-      int maxdis = 0;
-      for (size_t i = 0; i < zero.size(); ++i) {
-        int diff = zero[i].right - zero[i].left;
-        maxdis = diff > maxdis ? diff : maxdis;
+      int maxdiff = 0;
+      for (iter = pos.begin(); iter != pos.end(); ++iter) {
+        int diff = iter->second - iter->first;
+        maxdiff = diff > maxdiff ? diff : maxdiff;
       }
-      for (size_t i = 1; i < positive.size(); ++i) {
-        int diff = positive[i].right - positive[i].left;
-        maxdis = diff > maxdis ? diff : maxdis;
-      }
-      for (size_t i = 1; i < negative.size(); ++i) {
-        int diff = negative[i].right - negative[i].left;
-        maxdis = diff > maxdis ? diff : maxdis;
-      }
-      return maxdis;
+
+      return maxdiff;
     }
 };
 
